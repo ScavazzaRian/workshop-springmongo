4915f392748dceaba8be7d4fd7c9137c7513b630
ScavazzaRian <rianscavazza.git@gmail.com>
2026-02-24 13:45:10 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/example/workshopmongo/config/Instantiation.java
+++ src/main/java/com/example/workshopmongo/config/Instantiation.java
@@ -1,29 +1,40 @@
 package com.example.workshopmongo.config;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.example.workshopmongo.domain.Post;
 import com.example.workshopmongo.domain.User;
+import com.example.workshopmongo.repository.PostRepository;
 import com.example.workshopmongo.repository.UserRepository;
 
 @Configuration
-public class Instantiation implements CommandLineRunner{
+public class Instantiation implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
-	
-	
+
+	@Autowired
+	private PostRepository postRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
 		userRepository.deleteAll();
-		
+		postRepository.deleteAll();
+
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		
+
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+		Post post1 = new Post(null, LocalDate.now(), "Partiu viagem", "Vou viajar para Sao Paulo", maria);
+		Post post2 = new Post(null, LocalDate.now(), "Bom dia", "Acordei feliz hoje!", maria);
+
+		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
 }
---
+++ src/main/java/com/example/workshopmongo/domain/Post.java
@@ -0,0 +1,89 @@
+package com.example.workshopmongo.domain;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Post implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private String id;
+	private LocalDate date;
+	private String title;
+	private String body;
+
+	private User user;
+
+	public Post() {
+
+	}
+
+	public Post(String id, LocalDate date, String title, String body, User user) {
+		super();
+		this.id = id;
+		this.date = date;
+		this.title = title;
+		this.body = body;
+		this.user = user;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Post other = (Post) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
